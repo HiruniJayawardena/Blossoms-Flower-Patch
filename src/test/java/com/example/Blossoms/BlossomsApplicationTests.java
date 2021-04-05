fa207280e1bade3843124acc4cfa0ba80c1ac067
HiruniJayawardena <hiruniejayawardena@gmail.com>
2021-04-05 18:06:41 +0530

    first commit

---
+++ src/test/java/com/example/Blossoms/BlossomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blossoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlossomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
